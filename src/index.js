559feec61260fa908a0c5c0526f7d23ca3aa2f74
arty63 <artjom63@mail.ru>
2022-01-09 16:19:03 +0400

    feat: add reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+        let strNumber = String(Math.abs(n));
+        let reversed = '';
+        for (let i=1; i<=strNumber.length; i += 1){
+            reversed += strNumber[strNumber.length-i]
+        }return reversed
 }
